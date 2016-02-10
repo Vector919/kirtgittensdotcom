19552fb4a821a978ec71f166189f1a26c927ff5f
god <damagecontrolsoftware@gmail.com>
2016-02-10 01:28:03 -0500

    v3

---
+++ assets/js/dynamic.js
@@ -0,0 +1,35 @@
+// if you're looking at this, I'm aware this is bad engineering, don't decide to hire me based on this obvious garbage. k, thx?
+$("#item1").hover(function() {
+    $("#details-1").show('slow');
+},
+                 function(){
+    $("#details-1").hide('slow');
+});
+
+$("#item2").hover(function() {
+    $("#details-2").show('slow');
+},
+                 function(){
+    $("#details-2").hide('slow');
+});
+
+$("#item3").hover(function() {
+    $("#details-3").show('slow');
+},
+                 function(){
+    $("#details-3").hide('slow');
+});
+
+$("#item4").hover(function() {
+    $("#details-4").show('slow');
+},
+                 function(){
+    $("#details-4").hide('slow');
+});
+
+$("#item5").hover(function() {
+    $("#details-5").show('slow');
+},
+                 function(){
+    $("#details-5").hide('slow');
+});
